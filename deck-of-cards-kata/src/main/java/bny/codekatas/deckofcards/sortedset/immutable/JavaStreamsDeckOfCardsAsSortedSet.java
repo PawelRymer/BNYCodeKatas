066350270cd59fd5ce6eee0ec2627b4dea2528fd
PawelRymer <pawelrymer+github@gmail.com>
2026-09-22 15:44:13 +0200

    Complete Deck of Cards SortedSet with Java Stream

--- deck-of-cards-kata/src/main/java/bny/codekatas/deckofcards/sortedset/immutable/JavaStreamsDeckOfCardsAsSortedSet.java
+++ deck-of-cards-kata/src/main/java/bny/codekatas/deckofcards/sortedset/immutable/JavaStreamsDeckOfCardsAsSortedSet.java
@@ -16,14 +16,7 @@
 
 package bny.codekatas.deckofcards.sortedset.immutable;
 
-import java.util.Collections;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -32,6 +25,8 @@ import java.util.stream.IntStream;
 import bny.codekatas.deckofcards.Card;
 import bny.codekatas.deckofcards.Rank;
 import bny.codekatas.deckofcards.Suit;
+
+import static java.util.stream.Collectors.*;
 
 public class JavaStreamsDeckOfCardsAsSortedSet
 {
@@ -48,15 +43,25 @@ public class JavaStreamsDeckOfCardsAsSortedSet
      */
     public JavaStreamsDeckOfCardsAsSortedSet()
     {
-        this.cards = null;
-        this.cardsBySuit = null;
+        this.cards = Collections.unmodifiableSortedSet(
+                Card.streamCards().sorted().collect(Collectors.toCollection(TreeSet::new)));
+        this.cardsBySuit = Map.copyOf(this.cards.stream()
+                .collect(Collectors.groupingBy(
+                        Card::suit,
+                        collectingAndThen(
+                                Collectors.toCollection(TreeSet::new),
+                                Collections::unmodifiableSortedSet))));
     }
 
     public Deque<Card> shuffle(Random random)
     {
         // TODO Shuffle the deck 3 times with the Random parameter and push the shuffled cards onto an ArrayDeque
         // Hint: Look at IntStream.range() or IntStream.rangeClosed() and Collections.shuffle()
-        return null;
+        var mutableCardsDeck = new ArrayList<>(this.cards);
+        IntStream.rangeClosed(1, 3).forEach(i -> Collections.shuffle(mutableCardsDeck, random));
+        var deque = new ArrayDeque<Card>();
+        mutableCardsDeck.forEach(deque::push);
+        return deque;
     }
 
     public Set<Card> deal(Deque<Card> deque, int count)
@@ -78,45 +83,45 @@ public class JavaStreamsDeckOfCardsAsSortedSet
             int cardsPerHand)
     {
         // TODO Deal the number of hands with the cardsPerHand into an "immutable" List<Set<Card>>
-        return null;
+        return IntStream.range(0, hands).mapToObj(i -> this.deal(shuffled, cardsPerHand)).toList();
     }
 
     public SortedSet<Card> diamonds()
     {
         // TODO return all diamonds as an "Immutable" SortedSet
-        return null;
+        return this.cardsBySuit.get(Suit.DIAMONDS);
     }
 
     public SortedSet<Card> hearts()
     {
         // TODO return all hearts as an "Immutable" SortedSet
-        return null;
+        return this.cardsBySuit.get(Suit.HEARTS);
     }
 
     public SortedSet<Card> spades()
     {
         // TODO return all spades as an "Immutable" SortedSet
-        return null;
+        return this.cardsBySuit.get(Suit.SPADES);
     }
 
     public SortedSet<Card> clubs()
     {
         // TODO return all clubs as an "Immutable" SortedSet
-        return null;
+        return this.cardsBySuit.get(Suit.CLUBS);
     }
 
     public Map<Suit, Long> countsBySuit()
     {
         // TODO return the count of cards by Suit
         // Hint: Look at Collectors.groupingBy() and Collectors.counting()
-        return null;
+        return this.cards.stream().collect(Collectors.groupingBy(Card::suit, Collectors.counting()));
     }
 
     public Map<Rank, Long> countsByRank()
     {
         // TODO return the count of cards by Rank
         // Hint: Look at Collectors.groupingBy() and Collectors.counting()
-        return null;
+        return this.cards.stream().collect(Collectors.groupingBy(Card::rank, Collectors.counting()));
     }
 
     public SortedSet<Card> getCards()
